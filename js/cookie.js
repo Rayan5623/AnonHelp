5f8463133480c419843013e35721c158de9f43dd
Rayan5623 <rayan.crine4@gmail.com>
2022-08-26 17:39:06 +0200

    edit win

--- js/cookie.js
+++ js/cookie.js
@@ -1,35 +1,38 @@
 // fonte: https://www.w3schools.com/js/js_cookies.asp
 
 function setCookie(cname, cvalue, exdays) { // crea il cookie
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) { // ritorna il valore del cookie
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
   }
-  return "";
-}
-
-function checkCookie() { // verifica se il cookie è stato settato, il controllo del cookie avviene in questa funzione
-  let user = getCookie("username");
-  if (user != "") {
-    alert("Welcome again " + user);
-  } else {
-    user = prompt("Please enter your name:", "");
-    if (user != "" && user != null) {
-      setCookie("username", user, 365);
+  
+  function getCookie(cname) { // ritorna il valore del cookie
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
     }
+    return "";
   }
-} 
+  
+  function checkCookie() { // verifica se il cookie è stato settato, il controllo del cookie avviene in questa funzione
+    let user = document.getElementById("user").value;
+    //let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+    window.open("./chat.html");
+  } 
+
