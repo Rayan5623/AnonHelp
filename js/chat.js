7fded2c559b5c6637183334d27c90aa7734285a6
Rayan5623 <60489699+Rayan5623@users.noreply.github.com>
2023-06-23 15:49:15 +0200

    edit Fis

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,21 @@
+// js di prova
+function getUser(user){
+        
+    // document.getElementById("prova").innerHTML = {user};
+     var user = document.getElementById("user").value;
+     if(user==null){
+     user = "Anonymous";
+     alert(user);
+     window.open("./chat.html");
+     }else if(user!=null){
+         alert(user);
+         window.open("./chat.html");
+     }
+    //alert(document.getElementById("user").value); .value -> è usato per restituire il valore contenuto nell'input "username"
+    return user;
+
+ }
+ 
 function pushuser(user){
     getUser();
     document.getElementById("userplace").innerHTML = user;
